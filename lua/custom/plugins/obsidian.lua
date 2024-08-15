dea4f76379e4cd5c2d638939ae82dbd1fa0b73ab
Salman <m.salman86@gmail.com>
2024-08-15 11:24:21 -0400

    fix: obsidian plugin config

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -3,13 +3,8 @@ return {
     'epwalsh/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
-    -- ft = 'markdown',
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     event = {
-      -- refer to `:h file-pattern` for more examples
-      'BufReadPre '
-        .. vim.fn.expand '~'
-        .. '/second-brain/**/*.md',
+      'BufReadPre ' .. vim.fn.expand '~' .. '/second-brain/**/*.md',
       'BufNewFile ' .. vim.fn.expand '~' .. '/second-brain/**/*.md',
     },
     dependencies = {
@@ -23,5 +18,17 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require('obsidian').setup(opts)
+
+      -- Set conceallevel for Markdown files in the Obsidian vault
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = vim.fn.expand '~' .. '/second-brain/**/*.md',
+        callback = function()
+          vim.opt_local.conceallevel = 2
+        end,
+        group = vim.api.nvim_create_augroup('ObsidianMarkdownConceal', { clear = true }),
+      })
+    end,
   },
 }
